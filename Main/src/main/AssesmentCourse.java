a31d61efff2e3c065963a281f691311838d61285
9T9DEVIL <mughalhammadali9@gmail.com>
2021-06-14 11:35:52 +0500

    .

--- Main/src/main/AssesmentCourse.java
+++ Main/src/main/AssesmentCourse.java
@@ -77,6 +77,11 @@ public class AssesmentCourse extends javax.swing.JFrame {
         });
 
         jButton2.setText("OK");
+        jButton2.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton2ActionPerformed(evt);
+            }
+        });
 
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
@@ -151,6 +156,10 @@ TeacherMenuFrame t= new TeacherMenuFrame();
   t.setVisible(true);
 this.setVisible(false);// TODO add your handling code here:
     }//GEN-LAST:event_jButton1ActionPerformed
+
+    private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jButton2ActionPerformed
 
     /**
      * @param args the command line arguments
